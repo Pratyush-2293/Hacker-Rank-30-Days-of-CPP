66915396e0fb08422495a4b95f4e4d9e94789fab
Pratyush-2293 <117383222+Pratyush-2293@users.noreply.github.com>
2022-11-14 22:57:47 +0530

    Update Day_8.cpp

--- Day_8.cpp
+++ Day_8.cpp
@@ -7,7 +7,24 @@
 using namespace std;
 
 
-int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+int main(){
+    int n;
+    string name, number, query;
+    map<string, string>phoneBook;
+
+    cin>>n;
+    for(int j=0;j<n;j++){
+        cin>>name>>number;
+        phoneBook[name] = number;
+    }
+
+    while(cin>>query){
+        if(phoneBook.find(query) != phoneBook.end()){
+            cout<<query<<"="<<phoneBook.find(query)->second<<endl;
+        } else{
+            cout<<"Not Found"<<endl;
+        }
+    }
+
     return 0;
 }
