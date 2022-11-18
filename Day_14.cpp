7c7b4df1ad625c64cb5fa548d029ea28aacaff05
Pratyush-2293 <117383222+Pratyush-2293@users.noreply.github.com>
2022-11-19 00:27:18 +0530

    Create Day_14.cpp

---
+++ Day_14.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class Difference {
+    private:
+    vector<int> elements;
+  
+  	public:
+  	int maximumDifference;
+
+	// Add your code here
+
+};
