77ae465712cf71e9e422136febbbd98a8230d428
Pratyush-2293 <117383222+Pratyush-2293@users.noreply.github.com>
2022-11-23 22:42:45 +0530

    Create Day_19.cpp

---
+++ Day_19.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+class AdvancedArithmetic{
+    public:
+        virtual int divisorSum(int n)=0;
+};
+class Calculator : public AdvancedArithmetic {
+public:
+    int divisorSum(int n) {
+        return 0;
+    }
+};
+
+int main(){
