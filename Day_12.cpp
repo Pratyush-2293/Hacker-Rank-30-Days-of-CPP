d4555b84ef4586b27185f842a78d43627e294c7f
Pratyush-2293 <117383222+Pratyush-2293@users.noreply.github.com>
2022-11-29 02:29:54 +0530

    Update Day_12.cpp

--- Day_12.cpp
+++ Day_12.cpp
@@ -23,7 +23,7 @@ class Person{
 
 class Student :  public Person{
 	private:
-		vector<int> testScores;  
+		vector<int> testscore;  
 	public:
         /*	
         *   Class Constructor
@@ -35,12 +35,55 @@ class Student :  public Person{
         *   scores - An array of integers denoting the Person's test scores.
         */
         // Write your constructor here
-
+        Student(string firstname, string lastname, int identification, vector<int>scores):
+        Person(firstname, lastname, identification){
+            testscore = scores;
+        }
+    
         /*	
         *   Function Name: calculate
         *   Return: A character denoting the grade.
         */
         // Write your function here
+        char calculate(){
+            int avg=0;
+            for(int i=0; i<testscore.size();i++){
+                avg += testscore[i];
+            }
+            avg = avg/testscore.size();
+            
+            if(avg>=90){
+                return 'O';
+            } else if(avg>=80){
+                return 'E';
+            } else if (avg>=70){
+                return 'A';
+            } else if (avg>=55){
+                return 'P';
+            } else if (avg>=40){
+                return 'D';
+            } else {
+                return 'T';
+            }
+        }
 };
 
 int main() {
+	string firstName;
+  	string lastName;
+	int id;
+  	int numScores;
+	cin >> firstName >> lastName >> id >> numScores;
+  	vector<int> scores;
+  	for(int i = 0; i < numScores; i++){
+	  	int tmpScore;
+	  	cin >> tmpScore;
+		scores.push_back(tmpScore);
+	}
+	Student* s = new Student(firstName, lastName, id, scores);
+	s->printPerson();
+	cout << "Grade: " << s->calculate() << "\n";
+	return 0;
+}
+
+//edit: fixed issue where some test case conditions were not met.
